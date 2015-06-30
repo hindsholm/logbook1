5d9c955918304669295fc6a53d7686517b7dd40b
Morten Hindsholm <morten@hindsholm.dk>
2015-06-30 12:04:47 +0200

    Now uses  constructor for creating promise

--- app/components/gpx/gpx-service.js
+++ app/components/gpx/gpx-service.js
@@ -8,26 +8,26 @@ angular.module('logbook')
         'use strict';
 
         function list() {
-            var deferred = $q.defer();
-            $http.get(TRACK_PATH).success(function (data) {
-                var dom = new DOMParser().parseFromString(data, 'text/html'),
-                    gpxs = [];
-                angular.forEach(dom.links, function (link) {
-                    var href = link.getAttribute('href'),
-                        isGpx = href.match(/([^/]+)\.gpx$/);
-                    if (isGpx) {
-                        gpxs.push({
-                            file: href,
-                            name: isGpx[1].replace(/(\d{4})-?(\d{2})-?(\d{2})(.*)/, '$1-$2-$3$4') || isGpx[1]
-                        });
-                    }
+            return $q(function (resolve) {
+                $http.get(TRACK_PATH).success(function (data) {
+                    var dom = new DOMParser().parseFromString(data, 'text/html'),
+                        gpxs = [];
+                    angular.forEach(dom.links, function (link) {
+                        var href = link.getAttribute('href'),
+                            isGpx = href.match(/([^/]+)\.gpx$/);
+                        if (isGpx) {
+                            gpxs.push({
+                                file: href,
+                                name: isGpx[1].replace(/(\d{4})-?(\d{2})-?(\d{2})(.*)/, '$1-$2-$3$4') || isGpx[1]
+                            });
+                        }
+                    });
+                    gpxs.sort(function (a, b) {
+                        return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+                    });
+                    resolve(gpxs);
                 });
-                gpxs.sort(function (a, b) {
-                    return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
-                });
-                deferred.resolve(gpxs);
             });
-            return deferred.promise;
         }
 
         return {
